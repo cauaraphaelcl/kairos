210096877ad0e1491186d649b14871964409352d
Cauã Raphael <cauaraphaelc@gmail.com>
2026-07-13 20:29:43 -0300

    add componente da sidebar add icons e logo + organização de pastas

--- frontend/assets/js/global.js
+++ frontend/assets/js/global.js
@@ -1,49 +1,41 @@
-const menuBtn = document.getElementById('burger-btn') as HTMLButtonElement;
-const asideCloseBtn = document.getElementById('aside-closebtn') as HTMLButtonElement;
-const aside = document.querySelector('aside') as HTMLElement;
-const logoImg = document.getElementById('kairos-logo') as HTMLImageElement;
-const navTexts = document.querySelectorAll<HTMLElement>('.nav-text');
-
-
-function isDesktop(): boolean {
+const menuBtn = document.getElementById('burger-btn');
+const asideCloseBtn = document.getElementById('aside-closebtn');
+const aside = document.querySelector('aside');
+const logoImg = document.getElementById('kairos-logo');
+const navTexts = document.querySelectorAll('.nav-text');
+function isDesktop() {
     return window.innerWidth >= 768;
 }
-
 // Colapsa/expande no desktop
 function toggleCollapse() {
     const collapsing = aside.classList.contains("md:w-1/6");
-
     aside.classList.toggle('md:w-1/6');
     aside.classList.toggle('md:w-28');
     aside.classList.toggle('is-collapsed');
     aside.classList.toggle('overflow-hidden');
     aside.classList.toggle('overflow-visible');
-
     navTexts.forEach((el) => el.classList.toggle('hidden'));
-
     logoImg.src = collapsing
         ? '/frontend/assets/images/logotipo-1.svg'
         : '/frontend/assets/images/logotipo-3.svg';
-    }
-
-    
+}
 // Mostra/esconde no mobile
 function toggleVisibility() {
     aside.classList.toggle('-translate-x-full');
 }
-
 // Estado inicial: escondido só no mobile
 if (!isDesktop()) {
     aside.classList.add('-translate-x-full');
 }
-
 // O botão de seta se comporta diferente dependendo da tela
 asideCloseBtn.addEventListener('click', () => {
     if (isDesktop()) {
         toggleCollapse();
-    } else {
+    }
+    else {
         toggleVisibility();
     }
 });
-
 menuBtn.addEventListener('click', toggleVisibility);
+export {};
+//# sourceMappingURL=global.js.map
